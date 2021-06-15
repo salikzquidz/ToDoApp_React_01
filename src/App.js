740610289442b3d27c8e00499d3b1b4885d24dc4
salikzquidz <salikinbaik@gmail.com>
2021-06-15 11:18:17 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo'
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
-function App() {
+function App(props) {
+  console.log(props.tasks)
+  const taskLists = props.tasks.map(x => <Todo name={x.name} id={x.id} completed={x.completed} key={x.id} />)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TodoMatic</h1>
+
+      <Form/>
+      <FilterButton/>
+      <FilterButton/>
+      <FilterButton/>
+      
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+
+      <ul
+        // role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+
+        {taskLists}
+      </ul>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const DATA = [
+  {name : 'Eat' ,completed : true ,id : 'todo-0'},
+  {name : 'Code' ,completed : false ,id : 'todo-1'},
+  {name : 'Repeat' ,completed : false ,id : 'todo-2'}
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App tasks = {DATA}/>,
+  
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
