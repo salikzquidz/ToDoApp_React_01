801d8578d9d65e2bfc921e5677a97d264d614ef0
salikzquidz <salikinbaik@gmail.com>
2021-06-17 03:23:19 +0800

    conditional rendering, callback props, editTask, edit and view template

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React, { useState } from 'react'
 import { nanoid } from 'nanoid';
 
 function App(props) {
-  const [tasks,setTasks] = useState(props.tasks)
+  const [tasks,setTasks] = useState([])
 
   //guna kaedah sendiri XD - guna find
   function toggleTaskCompleted(id){
@@ -35,14 +35,39 @@ function App(props) {
   }
 
   function addTask(name) {
+    if(name.length === 0){
+      return;
+    }
     const newTask = {name: name, id : "todo-" + nanoid(), completed : false}
     setTasks([...tasks, newTask])
     // console.log(tasks)
   }
 
+  //kaedah sendiri
+  // function editTask(id, newName){
+  //   const specificTask = tasks.find(task => task.id === id)
+  //   specificTask.name = newName;
+  //   console.log(tasks)
+  //   setTasks([...tasks])
+  // }
+
+  //kaedah mdnwebdocs
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   
   console.log(tasks)
-  const taskLists = tasks.map(task => <Todo name={task.name} id={task.id} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask}/>)
+  const taskLists = tasks.map(task => <Todo name={task.name} id={task.id} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask}
+                                        editTask={editTask}/>)
   
   const taskRemaining = tasks.length;
   const taskNouns = tasks.length === 1 ? 'task' : 'tasks';
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 
-const DATA = [
-  // {name : 'Eat' ,completed : true ,id : 'todo-0'},
-  // {name : 'Code' ,completed : false ,id : 'todo-1'},
-  // {name : 'Repeat' ,completed : false ,id : 'todo-2'}
-]
+// const DATA = [
+//   // {name : 'Eat' ,completed : true ,id : 'todo-0'},
+//   // {name : 'Code' ,completed : false ,id : 'todo-1'},
+//   // {name : 'Repeat' ,completed : false ,id : 'todo-2'}
+// ]
 
 ReactDOM.render(
-    <App tasks = {DATA}/>,
+    <App/>,
   
   document.getElementById('root')
 );
